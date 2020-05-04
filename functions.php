eccf8a84567e5b3298b9584f55fb1c1dc517c35e
Benjamin Jaume <47286801+BenjaminJaume@users.noreply.github.com>
2020-05-04 17:17:18 -0600

    Starting dev theme

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function load_stylesheet()
+{
+    wp_register_style('custom', get_template_directory_uri() . '/css/custom.css', '', false, 'all');
+    wp_enqueue_style('custom');
+
+    wp_register_style('modules', get_template_directory_uri() . '/css/modules.css', '', false, 'all');
+    wp_enqueue_style('modules');
+}
+add_action('wp_enqueue_scripts', 'load_stylesheet');
+
+function load_jquery()
+{
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', get_template_directory_uri() . '/js/vendor/jquery-3.4.1.min.js', '', '3.4.1', true);
+}
+add_action('wp_enqueue_scripts', 'load_jquery');
+
+
+function load_javascript()
+{
+    wp_enqueue_script('custom', get_template_directory_uri() . '/js/custom.js', 'jquery', false, true);
+}
+add_action('wp_enqueue_scripts', 'load_javascript');
