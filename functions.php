c05ffbbf454c3321c40983bea177b8517642171c
Benjamin Jaume <47286801+BenjaminJaume@users.noreply.github.com>
2020-05-05 11:34:11 -0600

    Updating theme

--- functions.php
+++ functions.php
@@ -20,6 +20,41 @@ add_action('wp_enqueue_scripts', 'load_jquery');
 
 function load_javascript()
 {
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/vendor/bootstrap.min.js', 'jquery', '4.3.1', true);
     wp_enqueue_script('custom', get_template_directory_uri() . '/js/custom.js', 'jquery', false, true);
 }
 add_action('wp_enqueue_scripts', 'load_javascript');
+
+// Add menu feature in WordPress
+add_theme_support('menus');
+
+// Add thumbnail feature in Wordpress
+add_theme_support('post-thumbnails');
+
+// Add templates feature in Wordpress
+add_theme_support('templates');
+
+// Add Boostrap Menu file with Wordpress
+function register_navwalker()
+{
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action('after_setup_theme', 'register_navwalker');
+
+register_nav_menus(
+    array(
+        'menu' => __("Menu", 'theme')
+    )
+);
+
+function console($data)
+{
+    echo "<script>console.log('" . json_encode($data) . "');</script>";
+}
+
+// turn off wysiwig for custom post types
+add_action('init', 'init_remove_support', 100);
+function init_remove_support()
+{
+    remove_post_type_support('page', 'editor');
+}
--- header.php
+++ header.php
@@ -11,7 +11,32 @@
 
 <body <?php body_class('main'); ?>>
 
-    <header>
-        <div class="container">
-        </div>
+    <header class="sticky-top" role="banner">
+        <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+            <div class="container">
+                <a class="navbar-brand mx-auto" href=<?php bloginfo('url'); ?> style="padding-left: 56px">
+                    <img src="<?php echo get_template_directory_uri() . '/pictures/logo.png' ?>" alt="" class="align-middle navbar-logo" />
+                    Salut
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <?php
+                wp_nav_menu(array(
+                    'theme_location'    => 'menu',
+                    'depth'             => 2,
+                    'container'         => 'div',
+                    'container_class'   => 'collapse navbar-collapse',
+                    'container_id'      => 'navbar',
+                    'menu_class'        => 'nav navbar-nav mx-auto',
+                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'            => new WP_Bootstrap_Navwalker(),
+                ));
+                ?>
+            </div>
+        </nav>
     </header>
+
+    <main role="main">
